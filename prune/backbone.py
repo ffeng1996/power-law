5d8835620bf7a6aa1a08805b92c098cc562ffb32
ffeng1996 <ffeng1017@gmail.com>
2019-12-13 14:56:44 +0800

    models

--- prune/backbone.py
+++ prune/backbone.py
@@ -69,3 +69,41 @@ class MLP(PruningModule):
         x = F.log_softmax(x, dim=1)
 
         return x
+
+class AlextNet(PruningModule):
+    def __int__(self, mask=False):
+        super(AlextNet,self).__init__()
+        linear = MaskedLinear if mask else nn.Linear
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 10),
+        )
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = nn.flatten(x, 1)
+        x = self.classifier(x)
+
+        return x
+
