f7605a9f7fbe539d45075d164dc8e8636e04f142
ffeng1996 <ffeng1017@gmail.com>
2019-12-13 14:17:38 +0800

    restructure

--- prune/weight_share.py
+++ prune/weight_share.py
@@ -3,9 +3,9 @@ import os
 
 import torch
 
-from prune.models import LeNet
-from prune.quantization import apply_weight_sharing
-import prune.util
+from net.models import LeNet
+from net.quantization import apply_weight_sharing
+import util
 
 parser = argparse.ArgumentParser(description='This program quantizes weight by using weight sharing')
 parser.add_argument('model', type=str, help='path to saved pruned model')
@@ -21,12 +21,12 @@ use_cuda = not args.no_cuda and torch.cuda.is_available()
 # Define the model
 model = torch.load(args.model)
 print('accuracy before weight sharing')
-prune.util.test(model, use_cuda)
+util.test(model, use_cuda)
 
 # Weight sharing
 apply_weight_sharing(model)
 print('accuacy after weight sharing')
-prune.util.test(model, use_cuda)
+util.test(model, use_cuda)
 
 # Save the new model
 os.makedirs('saves', exist_ok=True)
